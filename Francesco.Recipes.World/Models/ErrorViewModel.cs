59626f852ee2848334ce168b4bd4868b4838e9d3
Francesco D'Amico <fda145977@stud.gibb.ch>
2024-12-04 11:27:03 +0100

    InitialCommit

---
+++ Francesco.Recipes.World/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Francesco.Recipes.World.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
