87b0a9993cfe2ce486cbda6d266e80f0e263418b
Francesco D'Amico <fda145977@stud.gibb.ch>
2024-12-10 13:39:51 +0100

    Delete obsolete files

--- Francesco.Recipes.World/GlobalSuppressions.cs
+++
@@ -1,3 +0,0 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:Element should begin with upper-case letter", Justification = "Error pages should be accessible with their number and the naming must start with an underscore.", Scope = "namespaceanddescendants", Target = "Recruitment.Tool")]
