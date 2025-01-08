08c567b9ba583087528519f6955467a3c709b736
Francesco D'Amico <fda145977@stud.gibb.ch>
2025-01-08 11:30:50 +0100

    Some Configuration for this Project

--- Francesco.Recipes.World/Program.cs
+++ Francesco.Recipes.World/Program.cs
@@ -1,4 +1,26 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Francesco.Recipes.World.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+var configuration = builder.Configuration;
+
+services.AddLogging(
+    loggingBuilder =>
+        loggingBuilder.AddSerilog(
+            new LoggerConfiguration().ReadFrom.Configuration(configuration).CreateLogger()));
+
+
+var connectionString = builder.Configuration.GetConnectionString("FrancescosRecipesWorldDbContextConnection")
+                       ?? throw new InvalidOperationException("Connection string 'RecruitmentToolDbContextConnection' not found.");
+services.AddDbContext<FrancescosRecipesWorldDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<FrancescosRecipesWorldDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -18,8 +40,11 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();
 
 app.UseAuthorization();
+
+app.MapDefaultControllerRoute();
 
 app.MapControllerRoute(
     name: "default",
