6125eb82ee492e4d7bdd66794a894bd4256d82b3
Francesco D'Amico <fda145977@stud.gibb.ch>
2025-03-18 15:25:30 +0100

    Correct Models

--- Francesco.Recipes.World/Models/BackendModels/Recipe/Recipe.cs
+++ Francesco.Recipes.World/Models/BackendModels/Recipe/Recipe.cs
@@ -1,25 +1,24 @@
-﻿namespace Francesco.Recipes.World.Models.BackendModels.Recipe
-{
-    using Francesco.Recipes.World.Models.BackendModels.Category;
-    using Francesco.Recipes.World.Models.BackendModels.Favorit;
-    using Francesco.Recipes.World.Models.BackendModels.Instruction;
-    using Francesco.Recipes.World.Models.BackendModels.MediaFile;
-    using Francesco.Recipes.World.Models.BackendModels.RecipeIngredient;
+﻿namespace Francesco.Recipes.World.Models.BackendModels.Recipe;
 
-    public class Recipe : ITimeStampedEntity
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string? Description { get; set; }
-        public Difficulty Difficulty { get; set; }
-        public int Servings { get; set; }
-        public TimeSpan PreparationTime { get; set; }
-        public TimeSpan CookingTime { get; set; }
-        public bool IsFavorite { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime? ModifiedAt { get; set; }
-        public virtual ICollection<RecipeIngredient> RecipeIngredients { get; set; } = new List<RecipeIngredient>();
-        public virtual ICollection<Instruction> Instructions { get; set; } = new List<Instruction>();
-        public virtual ICollection<MediaFile> MediaFiles { get; set; } = new List<MediaFile>();
-        public virtual Favorit Favorit { get; set; } = new();
-    }
+using Francesco.Recipes.World.Models.BackendModels.Favorit;
+using Francesco.Recipes.World.Models.BackendModels.Instruction;
+using Francesco.Recipes.World.Models.BackendModels.MediaFile;
+using Francesco.Recipes.World.Models.BackendModels.RecipeIngredient;
+
+public class Recipe : ITimeStampedEntity
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string? Description { get; set; }
+    public Difficulty Difficulty { get; set; }
+    public int Servings { get; set; }
+    public TimeSpan PreparationTime { get; set; }
+    public TimeSpan CookingTime { get; set; }
+    public bool IsFavorite { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? ModifiedAt { get; set; }
+    public virtual ICollection<RecipeIngredient> RecipeIngredients { get; set; } = new List<RecipeIngredient>();
+    public virtual ICollection<Instruction> Instructions { get; set; } = new List<Instruction>();
+    public virtual ICollection<MediaFile> MediaFiles { get; set; } = new List<MediaFile>();
+    public virtual Favorit Favorit { get; set; } = new();
+}
