eb6d82a979662d354a8062224d1e53e8d29860fb
franc <franc@FRANCESCOSBUSIN>
2025-04-16 11:22:09 +0200

    Add HomeController with search endpoint for querying recipes

---
+++ Francesco.Recipes.World/Controller/HomeController/HomeController.cs
@@ -0,0 +1,25 @@
+﻿namespace Francesco.Recipes.World.Controller.Home
+{
+    using Francesco.Recipes.World.Repositories.Category;
+    using Francesco.Recipes.World.Repositories.Recipe;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class HomeController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IRecipeRepository _recipeRepository;
+
+        public HomeController(ICategoryRepository categoryRepository, IRecipeRepository recipeRepository)
+        {
+            _categoryRepository = categoryRepository;
+            _recipeRepository = recipeRepository;
+        }
+
+        [HttpGet("/Home/Search")]
+        public async Task<IActionResult> Search(string query)
+        {
+            var recipes = await _recipeRepository.GetRecipesBySearchQueryAsync(query);
+            return PartialView("_SearchResultsPartial", recipes);
+        }
+    }
+}
