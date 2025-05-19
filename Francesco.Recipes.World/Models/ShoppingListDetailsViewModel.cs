00cd006bf87138262ef39084a482b29c51b18a31
Francesco Lorenzo D'Amico <fda145977@stud.gibb.ch>
2025-05-19 13:24:38 +0200

    Create an initial ViewModel for the ShoppingList Detail

---
+++ Francesco.Recipes.World/Models/ShoppingListDetailsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Francesco.Recipes.World.Models.BackendModels.Shoppinglist;
+
+namespace Francesco.Recipes.World.Models
+{
+    public class ShoppingListDetailsViewModel
+    {
+        public ShoppingList ShoppingList { get; set; } = new ShoppingList();
+
+        public int RecipeCount { get; set; }
+    }
+}
