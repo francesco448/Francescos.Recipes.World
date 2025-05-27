203b8db1808068e0e79cd8755b1f02f4db2c3027
Francesco Lorenzo D'Amico <fda145977@stud.gibb.ch>
2025-05-27 12:01:06 +0200

    Delete unnecessary property ShoppingList

--- Francesco.Recipes.World/Models/ShoppingListDetailsViewModel.cs
+++ Francesco.Recipes.World/Models/ShoppingListDetailsViewModel.cs
@@ -1,14 +1,13 @@
 ﻿using Francesco.Recipes.World.Models.BackendModels.Recipe;
-using Francesco.Recipes.World.Models.BackendModels.Shoppinglist;
 
 namespace Francesco.Recipes.World.Models
 {
     public class ShoppingListDetailsViewModel
     {
-        public ShoppingList ShoppingList { get; set; } = new ShoppingList();
-
         public int RecipeCount { get; set; }
 
         public List<Recipe> RecipesInAnyShoppingList { get; set; } = new List<Recipe>();
+
+        public Dictionary<Guid, Guid> RecipeIngredientToShoppingListMap { get; set; } = new Dictionary<Guid, Guid>();
     }
 }
