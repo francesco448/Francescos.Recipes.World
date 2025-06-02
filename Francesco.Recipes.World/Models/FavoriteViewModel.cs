c9f3f2b3436f5ed889d1d269c000be25f825a4b9
Francesco Lorenzo D'Amico <fda145977@stud.gibb.ch>
2025-06-02 09:03:19 +0200

    Make an viewmodel for the favorit sort

---
+++ Francesco.Recipes.World/Models/FavoriteViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Francesco.Recipes.World.Models.BackendModels.Recipe;
+
+namespace Francesco.Recipes.World.Models
+{
+    public class FavoriteViewModel
+    {
+        public IEnumerable<Recipe> FavoriteRecipes { get; set; } = new List<Recipe>();
+
+        public string SortOrder { get; set; } = "newest";
+
+        public bool HasFavorites => FavoriteRecipes.Any();
+
+        public string SortOrderDisplayText => SortOrder == "oldest" ? "Älteste Favorits" : "Neueste Favorits";
+    }
+}
