cac83e38a8f2e3350714205cd4a67f2b55a8020b
Francesco Lorenzo D'Amico <francescodamico448@gmail.com>
2026-03-02 15:59:48 +0100

    Introduce IFavoritable and adopt across Recipe and view models

---
+++ Francesco.Recipes.World/Models/FavoritButtonViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Francesco.Recipes.World.Models
+{
+    public class FavoritButtonViewModel : IFavoritable
+    {
+        public Guid Id { get; set; }
+
+        public bool IsFavorite { get; set; }
+    }
+}
---
+++ Francesco.Recipes.World/Models/RecipeCardViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Francesco.Recipes.World.Models
+{
+    public class RecipeCardViewModel : IFavoritable
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public TimeSpan CookingTime { get; set; }
+
+        public bool IsFavorite { get; set; }
+
+        public byte[]? ImageData { get; set; }
+
+        public string? MimeType { get; set; }
+    }
+}
